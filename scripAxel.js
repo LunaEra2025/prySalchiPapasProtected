244869c80db188a1e693ef92c14ed219793f1a0a
tade <vaxel7045@gmail.com>
2026-09-14 10:39:44 -0600

    Agregando codigo

---
+++ scripAxel.js
@@ -0,0 +1,15 @@
+
+// Programa de saludo
+
+// Mensaje de bienvenida
+let nombre = "Axel";
+
+// Mostrar saludo en la consola
+console.log("================================");
+console.log("       PROGRAMA DE SALUDO");
+console.log("================================");
+console.log("Hola, soy " + nombre);
+console.log("¡Mucho gusto!");
+console.log("================================");
+
+
